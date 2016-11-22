b73de65887430cb2db2d6cfc1e763216c2f7589f
RumbleFrog <xxlosttechxx@gmail.com>
2016-11-21 19:19:56 -0500

    Update calendar_content.php

--- assets/calendar_content.php
+++ assets/calendar_content.php
@@ -2,7 +2,7 @@
 
 require("Calendar.php");
 
-$db = new PDO("mysql:dbname=test;host=127.0.0.1", "root", "wysiwyg");
+$db = new PDO("mysql:dbname=test;host=127.0.0.1", "root", "wysiwyg") or die("Fail to connect");
 
 $events = $db->query("SELECT * FROM test1 ORDER BY `date_start`", PDO::FETCH_ASSOC);
 
